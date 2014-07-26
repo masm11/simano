ed78dec51a31bf6b9daf41857b3bf7b5854e9136
Yuuki Harano <masm@flowernet.gr.jp>
2014-07-26 21:23:44 +0900

    new repository.

---
+++ simano.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <gtk/gtk.h>
+
+static gchar *server = NULL;
+static gint port = 0;
+
+static int sock = -1;
+static GIOChannel *channel = NULL;
+static guint watch = 0;
+
+static GtkStatusIcon *icon;
+
+static void refresh_connection(void);
+
+static void popup(const char *fmt, ...)
+{
+    char msg[1024];
+    
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(msg, sizeof msg, fmt, ap);
+    va_end(ap);
+    
+    GtkWidget *dialog = gtk_dialog_new_with_buttons(
+	    "Simple Mail Notifier", NULL,
+	    GTK_DIALOG_MODAL,
+	    "Retry", GTK_RESPONSE_ACCEPT,
+	    NULL);
+    gtk_widget_show(dialog);
+    
+    GtkWidget *label = gtk_label_new(msg);
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+    gtk_widget_show(label);
+    
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+}
+
+static void update(void)
+{
+    char buf[1];
+    switch (read(sock, buf, 1)) {
+    case -1:
+	if (errno == EINTR)
+	    return;
+	popup("read: %s", strerror(errno));
+	refresh_connection();
+	break;
+    case 0:
+	popup("read: Connection broken.");
+	refresh_connection();
+	break;
+    default:
+	gtk_status_icon_set_visible(icon, buf[0] != '0');
+	break;
+    }
+}
+
+static gboolean watch_cb(GIOChannel *source, GIOCondition condition, gpointer data)
+{
+    update();
+    return TRUE;
+}
+
+static void refresh_connection(void)
+{
+ retry:
+    if (watch != 0) {
+	g_source_remove(watch);
+	watch = 0;
+    }
+    if (channel != NULL) {
+	g_io_channel_unref(channel);
+	channel = NULL;
+    }
+    if (sock >= 0) {
+	close(sock);
+	sock = -1;
+    }
+    
+    struct addrinfo hint, *ai;
+    
+    memset(&hint, 0, sizeof hint);
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = 0;
+    
+    char svc[16];
+    snprintf(svc, sizeof svc, "%d", port);
+    
+    int err = getaddrinfo(server, svc, &hint, &ai);
+    if (err != 0) {
+	popup("getaddrinfo: %s", gai_strerror(err));
+	goto retry;
+    }
+    
+    sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sock == -1) {
+	popup("socket: %s", strerror(errno));
+	goto retry;
+    }
+    
+    if (connect(sock, ai->ai_addr, ai->ai_addrlen) == -1) {
+	popup("connect: %s", strerror(errno));
+	goto retry;
+    }
+    
+    channel = g_io_channel_unix_new(sock);
+    watch = g_io_add_watch(channel, G_IO_IN, watch_cb, NULL);
+}
+
+static GOptionEntry entries[] = {
+    { "server", 's', 0, G_OPTION_ARG_STRING, &server, "server hostname", NULL },
+    { "port",   'p', 0, G_OPTION_ARG_INT,    &port,   "server port",     NULL },
+    { NULL },
+};
+
+static void usage(void)
+{
+    g_print("Usage: simano -s <server> -p <port>\n");
+    exit(1);
+}
+
+int main(int argc, char **argv)
+{
+    if (!gtk_init_with_args(&argc, &argv, NULL, entries, NULL, NULL)) {
+	g_print("Invalid option.\n");
+	usage();
+    }
+    
+    if (server == NULL || port == 0)
+	usage();
+    
+    icon = gtk_status_icon_new_from_icon_name("xfce-newmail");
+    
+    refresh_connection();
+    
+    gtk_main();
+    
+    return 0;
+}
---
+++ simanod.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <dirent.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/inotify.h>
+
+#define MAILDIR "Maildir"
+
+static int check(const char *path)
+{
+    DIR *dir;
+    
+    if ((dir = opendir(path)) == NULL) {
+	perror(path);
+	return 0;
+    }
+    
+    struct dirent *ep;
+    int found = 0;
+    while ((ep = readdir(dir)) != NULL) {
+	char *p = strstr(ep->d_name, ":2,");
+	if (p != NULL) {
+	    if (strchr(p, 'S') == NULL)
+		found = 1;
+	}
+    }
+    
+    closedir(dir);
+    
+    return found;
+}
+
+static void send_status(int sock, int has_newmail)
+{
+    char data[1];
+    data[0] = has_newmail ? '1' : '0';
+    write(sock, data, 1);
+}
+
+static void service(int sock)
+{
+    char path[1024];
+    
+    snprintf(path, sizeof path, "%s/%s", getenv("HOME"), MAILDIR);
+    
+    int in = inotify_init();
+    if (in == -1) {
+	perror("inotify_init");
+	exit(1);
+    }
+    
+    char newdir[1024];
+    snprintf(newdir, sizeof newdir, "%s/new", path);
+    if (inotify_add_watch(in, newdir, IN_CREATE|IN_DELETE|IN_MOVE) == -1) {
+	perror(newdir);
+	exit(1);
+    }
+    
+    char curdir[1024];
+    snprintf(curdir, sizeof curdir, "%s/cur", path);
+    if (inotify_add_watch(in, curdir, IN_CREATE|IN_DELETE|IN_MOVE) == -1) {
+	perror(curdir);
+	exit(1);
+    }
+    
+    int has_newmail = check(newdir) || check(curdir);
+    
+    send_status(sock, has_newmail);
+    
+    while (1) {
+	fd_set rfds;
+	
+	FD_ZERO(&rfds);
+	FD_SET(sock, &rfds);
+	FD_SET(in, &rfds);
+	
+	int fdw = (sock > in ? sock : in) + 1;
+	if (select(fdw, &rfds, NULL, NULL, NULL) == -1) {
+	    if (errno == EINTR)
+		continue;
+	    perror("select");
+	    exit(1);
+	}
+	
+	if (FD_ISSET(in, &rfds)) {
+	    char buf[1024];
+	    
+	    switch (read(in, buf, sizeof buf)) {
+	    case -1:
+		if (errno != EINTR) {
+		    perror("read");
+		    exit(1);
+		}
+		break;
+		
+	    case 0:
+		exit(0);
+		
+	    default:;
+		int new_hasnewmail = check(newdir) || check(curdir);
+		if (new_hasnewmail != has_newmail) {
+		    has_newmail = new_hasnewmail;
+		    send_status(sock, has_newmail);
+		}
+	    }
+	}
+	
+	if (FD_ISSET(sock, &rfds)) {
+	    char buf[1024];
+	    switch (read(sock, buf, sizeof buf)) {
+	    case 0:
+		exit(1);
+	    case -1:
+		if (errno != EINTR) {
+		    perror("read");
+		    exit(1);
+		}
+		break;
+	    }
+	}
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+	fprintf(stderr, "usage: simanod -p <port>\n");
+	exit(1);
+    }
+    int port = atoi(argv[2]);
+    
+    int sock = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sock == -1) {
+	perror("socket");
+	exit(1);
+    }
+    
+    struct sockaddr_in6 sa;
+    memset(&sa, 0, sizeof sa);
+    sa.sin6_family = AF_INET6;
+    sa.sin6_port = htons(port);
+    if (bind(sock, (struct sockaddr *) &sa, sizeof sa) == -1) {
+	perror("bind");
+	exit(1);
+    }
+    
+    if (listen(sock, 5) == -1) {
+	perror("listen");
+	exit(1);
+    }
+    
+    struct sigaction act;
+    
+    memset(&act, 0, sizeof act);
+    act.sa_handler = SIG_DFL;
+    act.sa_flags = SA_NOCLDWAIT;
+    sigaction(SIGCHLD, &act, NULL);
+    
+    memset(&act, 0, sizeof act);
+    act.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &act, NULL);
+    
+    daemon(0, 0);
+    
+    while (1) {
+	int salen = sizeof sa;
+	int s = accept(sock, (struct sockaddr *) &sa, &salen);
+	if (s == -1) {
+	    if (errno != EINTR) {
+		perror("accept");
+		exit(1);
+	    }
+	    
+	    continue;
+	}
+	
+	switch (fork()) {
+	case -1:
+	    perror("fork");
+	    break;
+	case 0:
+	    close(sock);
+	    service(s);
+	    break;
+	default:
+	    close(s);
+	    break;
+	}
+    }
+    
+    return 0;
+}
