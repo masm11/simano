dc8f17d2781db471795f7cf90ef73ad68a97e999
Yuuki Harano <masm@masm11.ddo.jp>
2016-07-25 21:16:11 +0900

    裏で勝手に retry するようにした。とりあえず SimanoConnection のみ。

--- android/app/src/main/java/jp/ddo/masm11/simano/SimanoConnection.java
+++ android/app/src/main/java/jp/ddo/masm11/simano/SimanoConnection.java
@@ -29,83 +29,85 @@ class SimanoConnection implements Runnable {
     
     @Override
     public void run() {
-	SocketChannel sock = null;
-	long lastWrite = 0;
-	
 	try {
-	    sock = SocketChannel.open();
-	    sock.configureBlocking(false);
-	    
-	    Log.i("conn", "connection start.");
-	    if (!sock.connect(new InetSocketAddress(hostname, port))) {
-		while (!sock.finishConnect())
-		    Thread.sleep(100);
-	    }
-	    Log.i("conn", "connection done.");
-	    
-	    sock.socket().setSoTimeout(0);
-	    
-	    // ひどい作りやな…
-	    ByteBuffer rbuf = ByteBuffer.allocate(1);
-	    ByteBuffer wbuf = ByteBuffer.allocate(1);
 	    while (true) {
-		rbuf.position(0);
-		rbuf.limit(1);
-		int r;
+		SocketChannel sock = null;
+		long lastWrite = 0;
+		
 		try {
-		    r = sock.read(rbuf);
-		} catch (SocketException e) {
-		    if (e.toString().contains("ETIMEDOUT")) {
-			Log.w("conn", "ETIMEDOUT", e);
-			continue;
-		    }
-		    throw e;
-		}
-		if (r == -1) {
-		    // connection closed.
-		    Log.i("conn", "conn closed.");
-		    setError("Connection broken.");
-		    break;
-		}
-		if (r > 0) {
-		    rbuf.position(0);
-		    char c = (char) rbuf.get();
-		    if (c == '0') {
-			Log.i("conn", "No new mail.");
-			setState(false);
-		    } else if (c == '1') {
-			Log.i("conn", "You have new mails.");
-			setState(true);
-		    }
-		}
-		
-		long now = System.currentTimeMillis();
-		if (now - lastWrite >= 60 * 1000) {
-		    Log.d("conn", "write keepalive.");
-		    wbuf.position(0);
-		    wbuf.limit(1);
-		    wbuf.put((byte) '0');
-		    wbuf.position(0);
-		    sock.write(wbuf);
+		    sock = SocketChannel.open();
+		    sock.configureBlocking(false);
+		    sock.socket().setSoTimeout(0);
 		    
-		    lastWrite = now;
+		    Log.i("conn", "connection start.");
+		    if (!sock.connect(new InetSocketAddress(hostname, port))) {
+			while (!sock.finishConnect())
+			    Thread.sleep(100);
+		    }
+		    Log.i("conn", "connection done.");
+		    
+		    // ひどい作りやな…
+		    ByteBuffer rbuf = ByteBuffer.allocate(1);
+		    ByteBuffer wbuf = ByteBuffer.allocate(1);
+		    while (true) {
+			rbuf.position(0);
+			rbuf.limit(1);
+			int r = sock.read(rbuf);
+			if (r == -1) {
+			    // connection closed.
+			    Log.i("conn", "conn closed.");
+			    setError("Connection broken.");
+			    break;
+			}
+			if (r > 0) {
+			    rbuf.position(0);
+			    char c = (char) rbuf.get();
+			    if (c == '0') {
+				Log.i("conn", "No new mail.");
+				setState(false);
+			    } else if (c == '1') {
+				Log.i("conn", "You have new mails.");
+				setState(true);
+			    }
+			}
+			
+			long now = System.currentTimeMillis();
+			if (now - lastWrite >= 60 * 1000) {
+			    Log.d("conn", "write keepalive.");
+			    wbuf.position(0);
+			    wbuf.limit(1);
+			    wbuf.put((byte) '0');
+			    wbuf.position(0);
+			    sock.write(wbuf);
+			    
+			    lastWrite = now;
+			}
+			
+			Thread.sleep(100);
+		    }
+		} catch (SocketException e) {
+		    Log.e("conn", "socketexception.", e);
+		    setError(e.toString());
+		} catch (IOException e) {
+		    Log.e("conn", "ioexception.", e);
+		    setError(e.toString());
+		} finally {
+		    if (sock != null) {
+			try {
+			    sock.close();
+			} catch (IOException e) {
+			    Log.e("conn", "close failed", e);
+			}
+			sock = null;
+		    }
 		}
 		
-		Thread.sleep(100);
+		Log.d("conn", "sleeping...");
+		Thread.sleep(60 * 1000);
+		Log.d("conn", "sleeping... done.");
 	    }
 	} catch (InterruptedException e) {
 	    Log.i("conn", "intr", e);
-	} catch (Exception e) {
-	    Log.e("conn", "exception.", e);
-	    setError(e.toString());
-	} finally {
-	    if (sock != null) {
-		try {
-		    sock.close();
-		} catch (IOException e) {
-		    Log.e("conn", "close failed", e);
-		}
-	    }
 	}
     }
     
