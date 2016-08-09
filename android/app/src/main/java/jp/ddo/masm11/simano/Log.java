91024cd547361c2eb15659b7954badfeccc8fcb7
Yuuki Harano <masm@masm11.ddo.jp>
2016-08-09 21:40:43 +0900

    log をファイルにも出力するようにした。

--- android/app/src/main/java/jp/ddo/masm11/simano/Log.java
+++ android/app/src/main/java/jp/ddo/masm11/simano/Log.java
@@ -1,82 +1,85 @@
 package jp.ddo.masm11.simano;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 class Log {
     static void d(String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.d(klass, method + "(): " + msg);
+	common(android.util.Log.DEBUG, null, fmt, args);
     }
     
     static void d(Throwable e, String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.d(klass, method + "(): " + msg, e);
+	common(android.util.Log.DEBUG, e, fmt, args);
     }
     
     static void i(String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.i(klass, method + "(): " + msg);
+	common(android.util.Log.INFO, null, fmt, args);
     }
     
     static void i(Throwable e, String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.i(klass, method + "(): " + msg, e);
+	common(android.util.Log.INFO, e, fmt, args);
     }
     
     static void w(String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.w(klass, method + "(): " + msg);
+	common(android.util.Log.WARN, null, fmt, args);
     }
     
     static void w(Throwable e, String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.w(klass, method + "(): " + msg, e);
+	common(android.util.Log.WARN, e, fmt, args);
     }
     
     static void e(String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.e(klass, method + "(): " + msg);
+	common(android.util.Log.ERROR, null, fmt, args);
     }
     
     static void e(Throwable e, String fmt, Object... args) {
-	String[] stkinf = getStackInfo();
-	String klass = stkinf[0];
-	String method = stkinf[1];
-	String msg = String.format(fmt, args);
-	android.util.Log.e(klass, method + "(): " + msg, e);
+	common(android.util.Log.ERROR, e, fmt, args);
     }
     
-    static String[] getStackInfo() {
-	StackTraceElement[] elems = Thread.currentThread().getStackTrace();
-/*
-	for (int i = 0; i < elems.length; i++) {
-	    StackTraceElement e = elems[i];
-	    android.util.Log.i("simano", "" + i + ": class: " + e.getClassName());
-	    android.util.Log.i("simano", "" + i + ": file: " + e.getFileName());
-	    android.util.Log.i("simano", "" + i + ": line: " + e.getLineNumber());
-	    android.util.Log.i("simano", "" + i + ": method: " + e.getMethodName());
+    private static void common(int priority, Throwable e, String fmt, Object... args) {
+	String[] stkinf = getStackInfo();
+	String klass = stkinf[0];
+	StringBuffer buf = new StringBuffer();
+	buf.append(stkinf[1]);
+	buf.append("(): ");
+	buf.append(String.format(fmt, args));
+	if (e != null) {
+	    buf.append('\n');
+	    buf.append(android.util.Log.getStackTraceString(e));
 	}
-*/
-	return new String[] { elems[4].getClassName().replace("jp.ddo.masm11.simano.", ""),
-			      elems[4].getMethodName() };
+	String msg = buf.toString();
+	
+	android.util.Log.println(priority, klass, msg);
+	log_to_file(priority, klass, msg);
+    }
+    
+    private static PrintWriter writer = null;
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    synchronized private static void log_to_file(int priority, String tag, String msg) {
+	if (writer == null) {
+	    try {
+		writer = new PrintWriter(new BufferedWriter(new FileWriter("/sdcard/Android/data/jp.ddo.masm11.simano/cache/log.txt", true)));
+		writer.println("================");
+		writer.flush();
+	    } catch (IOException e) {
+		android.util.Log.e("Log", "ioexception", e);
+	    }
+	}
+	if (writer != null) {
+	    String time = formatter.format(new Date());
+	    writer.println(time + " " + tag + ": " + msg);
+	    writer.flush();
+	}
+    }
+    
+    private static String[] getStackInfo() {
+	StackTraceElement[] elems = Thread.currentThread().getStackTrace();
+	
+	return new String[] { elems[5].getClassName().replace("jp.ddo.masm11.simano.", ""),
+			      elems[5].getMethodName() };
     }
 }
